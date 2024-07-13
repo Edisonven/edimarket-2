2357af71e98931279e8e0682f2abd59a0a94d307
Edison <aalevenegass@gmail.com>
2024-07-12 22:54:39 -0400

    Creando función para almacenar valoraciones en estado

--- Frontend/src/pages/myValorations/StarRating.jsx
+++ Frontend/src/pages/myValorations/StarRating.jsx
@@ -4,16 +4,23 @@ import { GoStarFill } from "react-icons/go";
 
 export function StarRating() {
   const [score, setScore] = useState(0);
+
+  const handleChooseValoration = (index) => {
+    setScore(index + 1);
+  };
+
   return (
     <div className="flex items-center gap-5 mt-3 sm:mt-0">
       {[...new Array(5)].map((_, index) => {
         return index >= score ? (
           <GoStar
+            onClick={() => handleChooseValoration(index)}
             key={index}
             className="scale-[2] cursor-pointer select-none text-gray-400"
           />
         ) : (
           <GoStarFill
+            onClick={() => handleChooseValoration(index)}
             key={index}
             className="scale-[2] cursor-pointer select-none text-teal-700"
           />
