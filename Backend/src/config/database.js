e88f9fbfe9200dccf9046767c078aa27f38b9f33
Edison <aalevenegass@gmail.com>
2024-07-19 19:41:30 -0400

    Intentando conección con base de datos vercel

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -3,12 +3,6 @@ import pkg from "pg";
 import "dotenv/config";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DB,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  allowExitOnIdle: true,
-});
-
+  connectionString: process.env.POSTGRES_URL,
+})
 export default pool;
