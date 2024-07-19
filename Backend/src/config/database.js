06d905d02c04916f0366e884ac8b22a9e38f84ff
Edison <aalevenegass@gmail.com>
2024-07-19 17:58:37 -0400

    Ajustando conexxión

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,16 +1,9 @@
-import pkg from 'pg';
 const { Pool } = pkg;
-import 'dotenv/config';
+import pkg from "pg";
+import "dotenv/config";
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.DB_PORT || 5432,
-  ssl: {
-    rejectUnauthorized: false // Ajusta esta configuración según los requisitos de tu base de datos
-  }
-});
+  connectionString: process.env.POSTGRES_URL,
+})
 
 export default pool;
