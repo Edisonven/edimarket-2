47619e8602f7722b4337539ee8dec3585ea6291b
Edison <aalevenegass@gmail.com>
2024-07-24 17:18:30 -0400

    Agregando loader en preguntas realizadas

--- Frontend/src/pages/myQuestions/MyQuestions.jsx
+++ Frontend/src/pages/myQuestions/MyQuestions.jsx
@@ -6,6 +6,7 @@ import { HiDotsVertical } from "react-icons/hi";
 import { QuestionModal } from "../../components/questionModal/QuestionModal";
 import { BsDot } from "react-icons/bs";
 import { CartAlert } from "../../components/cartAlert/CartAlert";
+import { Loader } from "../../components/loader/Loader";
 
 const DeleteIcon = forwardRef((props, ref) => (
   <div ref={ref}>
@@ -88,74 +89,85 @@ export function MyQuestions() {
     <section className="myquestions___container ">
       <h1 className="text-2xl font-semibold mb-5">Mis preguntas realizadas</h1>
       <div className="myquestions__body flex flex-col gap-5 bg-white shadow-sm rounded-md p-3 h-[500px]">
-        {product.length > 0 ? (
-          product.map((element) => {
-            return (
-              <div
-                className="border rounded-md shadow w-full max-w-[800px] mx-auto"
-                key={element?.producto_id}
-              >
-                <div
-                  onClick={() => handleProductDetail(element?.producto_id)}
-                  className="flex items-center gap-2 relative cursor-pointer p-[15px]"
-                >
-                  <figure className="border p-2 rounded-md shadow">
-                    <img
-                      className="w-[80px] h-[65px] object-cover "
-                      src={element?.imagen}
-                      alt=""
-                    />
-                  </figure>
-                  <div className="flex items-center justify-between w-full overflow-hidden h-[40px]">
-                    <p className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[150px] md:max-w-full ">
-                      {element?.titulo}
-                    </p>
-                    <DeleteIcon
-                      ref={element?.producto_id === productId ? iconRef : null}
-                      onClick={(e) => handleOpenModal(element?.producto_id, e)}
-                      className="cursor-pointer scale-[2] select-none hover:bg-slate-200 rounded-full mr-3 p-[3px]"
-                    />
-                    {element?.producto_id === productId ? (
-                      <QuestionModal
-                        handleGetProductWithQuestions={
-                          handleGetProductWithQuestions
-                        }
-                        setQuestionsDeleted={setQuestionsDeleted}
-                        setProductId={setProductId}
-                        productId={productId}
-                        confirmProductId={element?.producto_id}
-                        ref={modalRef}
-                      />
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                </div>
-                <hr className="" />
-                <div className="flex flex-col min-h-[50px]">
-                  {element?.preguntas.map((pregunta) => {
-                    return (
-                      <div
-                        key={pregunta?.id_pregunta}
-                        className="flex items-center gap-3 font-medium overflow-hidden h-full bg-gray-100 min-h-[50px]"
-                      >
-                        <p className="overflow-hidden text-ellipsis whitespace-nowrap max-w-[180px] sm:max-w-full flex items-center pl-[16px]">
-                          <BsDot className="scale-150" /> {pregunta?.pregunta}
-                        </p>
-                        <span className="text-sm text-gray-500">
-                          {pregunta?.fecha}
-                        </span>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })
+        {loading ? (
+          <Loader></Loader>
         ) : (
-          <div className="flex flex-col items-center justify-center h-full mt-6">
-            <h3>No has hecho preguntas aún.</h3>
-            <p>Cuando hagas preguntas aparecerán acá.</p>
+          <div className="flex flex-col gap-5">
+            {product.length > 0 ? (
+              product.map((element) => {
+                return (
+                  <div
+                    className="border rounded-md shadow w-full max-w-[800px] mx-auto"
+                    key={element?.producto_id}
+                  >
+                    <div
+                      onClick={() => handleProductDetail(element?.producto_id)}
+                      className="flex items-center gap-2 relative cursor-pointer p-[15px]"
+                    >
+                      <figure className="border p-2 rounded-md shadow">
+                        <img
+                          className="w-[80px] h-[65px] object-cover "
+                          src={element?.imagen}
+                          alt=""
+                        />
+                      </figure>
+                      <div className="flex items-center justify-between w-full overflow-hidden h-[40px]">
+                        <p className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[150px] md:max-w-full ">
+                          {element?.titulo}
+                        </p>
+                        <DeleteIcon
+                          ref={
+                            element?.producto_id === productId ? iconRef : null
+                          }
+                          onClick={(e) =>
+                            handleOpenModal(element?.producto_id, e)
+                          }
+                          className="cursor-pointer scale-[2] select-none hover:bg-slate-200 rounded-full mr-3 p-[3px]"
+                        />
+                        {element?.producto_id === productId ? (
+                          <QuestionModal
+                            handleGetProductWithQuestions={
+                              handleGetProductWithQuestions
+                            }
+                            setQuestionsDeleted={setQuestionsDeleted}
+                            setProductId={setProductId}
+                            productId={productId}
+                            confirmProductId={element?.producto_id}
+                            ref={modalRef}
+                          />
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </div>
+                    <hr className="" />
+                    <div className="flex flex-col min-h-[50px]">
+                      {element?.preguntas.map((pregunta) => {
+                        return (
+                          <div
+                            key={pregunta?.id_pregunta}
+                            className="flex items-center gap-3 font-medium overflow-hidden h-full bg-gray-100 min-h-[50px]"
+                          >
+                            <p className="overflow-hidden text-ellipsis whitespace-nowrap max-w-[180px] sm:max-w-full flex items-center pl-[16px]">
+                              <BsDot className="scale-150" />{" "}
+                              {pregunta?.pregunta}
+                            </p>
+                            <span className="text-sm text-gray-500">
+                              {pregunta?.fecha}
+                            </span>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <div className="flex flex-col items-center justify-center h-full mt-6">
+                <h3>No has hecho preguntas aún.</h3>
+                <p>Cuando hagas preguntas aparecerán acá.</p>
+              </div>
+            )}
           </div>
         )}
       </div>
