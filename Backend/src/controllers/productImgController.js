bff2259a2785064f05d8ee996ba5101fd6c1fec7
Edison <aalevenegass@gmail.com>
2024-07-26 15:44:33 -0400

    Ajustando funciones para subir imágenes

--- Backend/src/controllers/productImgController.js
+++ Backend/src/controllers/productImgController.js
@@ -1,20 +1,29 @@
 import fs from "node:fs";
 
 const sendLocalImg = async (req, res) => {
-  console.log(req.file);
-  saveImg(req.file);
-  res.send("holi");
+  try {
+    const newPath = saveImg(req.file);
+    res.send(`Imagen subida: ${newPath}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al subir la imagen");
+  }
 };
 
 const saveImg = (file) => {
-  const newPath = `./uploads/${file.originalname}`;
+  const newPath = path.resolve(uploadDir, file.originalname);
   fs.renameSync(file.path, newPath);
   return newPath;
 };
 
 const sendMultiImgs = async (req, res) => {
-  req.files.map(saveImg);
-  res.send("imágenes subidas");
+  try {
+    req.files.map(saveImg);
+    res.send("Imágenes subidas");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al subir las imágenes");
+  }
 };
 
 export const productImgController = {
