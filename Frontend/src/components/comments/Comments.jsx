79424a7a48dc308027e05e78e31c74e599940e2a
Edison <aalevenegass@gmail.com>
2024-07-25 23:31:30 -0400

    Agregando loader en calificaciones

--- Frontend/src/components/comments/Comments.jsx
+++ Frontend/src/components/comments/Comments.jsx
@@ -8,9 +8,11 @@ import { GoStarFill } from "react-icons/go";
 import { GoStar } from "react-icons/go";
 import { UserContext } from "../../context/UserContext";
 import { useParams } from "react-router-dom";
+import { Loader } from "../loader/Loader";
 
 export function Comments() {
-  const { userValorations, productById } = useContext(ProductContext);
+  const { userValorations, productById, setLoading, loading } =
+    useContext(ProductContext);
   const { userToken, user } = useContext(UserContext);
   const { id } = useParams();
   const parsedId = parseInt(id);
@@ -35,6 +37,8 @@ export function Comments() {
       return data;
     } catch (error) {
       console.error(error.message || "Error al obtener calificaicones");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -122,85 +126,91 @@ export function Comments() {
         {productId?.producto_id === productById?.producto_id ? (
           <div className="opiniones__container">
             <h3 className="my-3 font-medium">Opiniones</h3>
-            <div className="opiniones__body flex flex-col">
-              {userValorations.map((valoration) => (
-                <div key={valoration.id} className="">
-                  <div className="flex items-center justify-between mb-3">
-                    <div className="flex items-center gap-4 w-full ">
-                      <p className="text-sm font-medium">
-                        {" "}
-                        {valoration?.usuario.split(" ")[0]}{" "}
-                        {valoration?.usuario.split(" ")[1]?.charAt(0)}.
-                      </p>
-                      <span className="flex items-center gap-1">
-                        {[...new Array(5)].map((_, index) =>
-                          index < valoration?.calificacion ? (
-                            <GoStarFill
-                              className="star-icon text-sm sm:text-[15px] select-none text-teal-700 rounded-full"
-                              key={index}
-                            />
-                          ) : (
-                            <GoStar
-                              className="star-icon text-sm sm:text-[15px] select-none text-gray-400 rounded-full"
-                              key={index}
-                            />
-                          )
-                        )}
-                      </span>
+            {califications.length === 0 ? (
+              <Loader className="scale-[0.6]" />
+            ) : (
+              <div className="opiniones__body flex flex-col">
+                {userValorations.map((valoration) => (
+                  <div key={valoration.id} className="">
+                    <div className="flex items-center justify-between mb-3">
+                      <div className="flex items-center gap-4 w-full ">
+                        <p className="text-sm font-medium">
+                          {" "}
+                          {valoration?.usuario.split(" ")[0]}{" "}
+                          {valoration?.usuario.split(" ")[1]?.charAt(0)}.
+                        </p>
+                        <span className="flex items-center gap-1">
+                          {[...new Array(5)].map((_, index) =>
+                            index < valoration?.calificacion ? (
+                              <GoStarFill
+                                className="star-icon text-sm sm:text-[15px] select-none text-teal-700 rounded-full"
+                                key={index}
+                              />
+                            ) : (
+                              <GoStar
+                                className="star-icon text-sm sm:text-[15px] select-none text-gray-400 rounded-full"
+                                key={index}
+                              />
+                            )
+                          )}
+                        </span>
+                      </div>
+                      <div>
+                        <p className="text-xs text-gray-400">
+                          {valoration?.fecha}
+                        </p>
+                      </div>
                     </div>
                     <div>
-                      <p className="text-xs text-gray-400">
-                        {valoration?.fecha}
+                      <p className="font-normal pl-3">
+                        {valoration?.comentario}
                       </p>
-                    </div>
-                  </div>
-                  <div>
-                    <p className="font-normal pl-3">{valoration?.comentario}</p>
-                    <div className="flex items-center gap-3 mt-3">
-                      <div
-                        onClick={() => handleSendMyCalification(valoration)}
-                        className={`flex items-center gap-2 cursor-pointer hover:outline outline-teal-500 outline-1 rounded-xl px-2 py-[1px] select-none ${
-                          califications.some(
+                      <div className="flex items-center gap-3 mt-3">
+                        <div
+                          onClick={() => handleSendMyCalification(valoration)}
+                          className={`flex items-center gap-2 cursor-pointer hover:outline outline-teal-500 outline-1 rounded-xl px-2 py-[1px] select-none ${
+                            califications.some(
+                              (cal) =>
+                                cal.positiva === true &&
+                                cal.calificacion_id === valoration.id &&
+                                cal.usuario_id === user?.id
+                            )
+                              ? "bg-teal-500 shadow-md text-white"
+                              : ""
+                          }`}
+                        >
+                          {califications.some(
                             (cal) =>
                               cal.positiva === true &&
                               cal.calificacion_id === valoration.id &&
                               cal.usuario_id === user?.id
-                          )
-                            ? "bg-teal-500 shadow-md text-white"
-                            : ""
-                        }`}
-                      >
-                        {califications.some(
-                          (cal) =>
-                            cal.positiva === true &&
-                            cal.calificacion_id === valoration.id &&
-                            cal.usuario_id === user?.id
-                        ) ? (
-                          <BsFillHandThumbsUpFill />
-                        ) : (
-                          <BsHandThumbsUp />
-                        )}
+                          ) ? (
+                            <BsFillHandThumbsUpFill />
+                          ) : (
+                            <BsHandThumbsUp />
+                          )}
 
-                        <span className="text-sm font-medium">
-                          {
-                            califications.filter(
-                              (cal) =>
-                                cal.calificacion_id === valoration.id &&
-                                cal.positiva === true
-                            ).length
-                          }
-                        </span>
-                      </div>
-                      <div className="flex items-center gap-2 cursor-pointer hover:outline outline-teal-500 outline-1 rounded-xl px-2 select-none">
-                        <BsHandThumbsDown />
-                        <span>0</span>
+                          <span className="text-sm font-medium">
+                            {
+                              califications.filter(
+                                (cal) =>
+                                  cal.calificacion_id === valoration.id &&
+                                  cal.positiva === true
+                              ).length
+                            }
+                          </span>
+                        </div>
+                        <div className="flex items-center gap-2 cursor-pointer hover:outline outline-teal-500 outline-1 rounded-xl px-2 select-none">
+                          <BsHandThumbsDown />
+                          <span>0</span>
+                        </div>
                       </div>
                     </div>
+                    <hr className="my-5" />
                   </div>
-                  <hr className="my-5" />
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>
+            )}
           </div>
         ) : (
           <p className="text-center w-full my-8">
--- Frontend/src/components/loader/Loader.jsx
+++ Frontend/src/components/loader/Loader.jsx
@@ -1,22 +1,22 @@
-import { Blocks } from "react-loader-spinner"
-import Lottie from 'react-lottie';
-import animationData from "./loader-animation.json"
+import { Blocks } from "react-loader-spinner";
+import Lottie from "react-lottie";
+import animationData from "./loader-animation.json";
 
+export function Loader({ className }) {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-export function Loader() {
-
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    };
-
-    return (
-        <div className="flex flex-col justify-center items-center my-36">
-            <Lottie options={defaultOptions} height={200} width={200} />
-        </div>
-    )
+  return (
+    <div
+      className={`${className} flex flex-col justify-center items-center my-36`}
+    >
+      <Lottie options={defaultOptions} height={200} width={200} />
+    </div>
+  );
 }
