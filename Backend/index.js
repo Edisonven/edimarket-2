039cd97edb46b36bb87a4a0fa8574cbd632eeb11
Edison <aalevenegass@gmail.com>
2024-07-26 16:34:47 -0400

    Ajustando configuración

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,23 @@
 import "dotenv/config";
 import express, { json } from "express";
-const app = express();
 import cors from "cors";
+import multer from "multer";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 import userRoutes from "./src/routes/userRoutes.js";
 import categoriaRoutes from "./src/routes/categoriaRoutes.js";
 import productRoutes from "./src/routes/productRoutes.js";
 import favoritosRoutes from "./src/routes/favoritosRoutes.js";
 import carritoRoutes from "./src/routes/carritoRoutes.js";
 import ventaRoutes from "./src/routes/ventaRoutes.js";
+
+const app = express();
 const port = process.env.PORT || 3000;
-import multer from "multer";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
 app.use(json());
-
-const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
 
 const upload = multer({
   dest: join(CURRENT_DIR, "./uploads"),
@@ -27,12 +28,7 @@ const upload = multer({
 
 app.post("/upload-imgs", upload.single("images"), (req, res) => {
   console.log(req.file);
-
-  res.status(200).json({ message: "archivos subidos exitosamente" });
-});
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  res.status(200).json({ message: "Archivos subidos exitosamente" });
 });
 
 app.use("/usuarios", userRoutes);
@@ -45,4 +41,9 @@ app.use("/preguntas", productRoutes);
 
 app.get("*", (_, res) => {
   res.status(404).send("Ruta no encontrada");
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
